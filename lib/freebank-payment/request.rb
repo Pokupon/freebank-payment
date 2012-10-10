c41a073200570b68fc1b8c27bff384cb4ab4a0d8
Alexei Curguzchin <alexei.curguzchin@pokupon.ua>
2012-10-10 14:22:53 +0300

    created and tested FreebankPayment::Request

--- lib/freebank-payment/request.rb
+++ lib/freebank-payment/request.rb
@@ -1,3 +1,54 @@
+require 'uri'
+require 'digest/sha1'
+
 class FreebankPayment::Request
 
+  attr_accessor :pay_amount, :pay_order, :pay_goal, :pay_success_url, :pay_fail_url, :pay_result_url, :provider
+  attr_accessor :secret_key
+
+  def params
+    validate!
+    result = {
+        :pay_amount => sprintf('%.2f', pay_amount),
+        :pay_order => pay_order,
+        :pay_goal => pay_goal[0, 160],
+        :pay_success_url => pay_success_url,
+        :pay_fail_url => pay_fail_url,
+        :pay_result_url => pay_result_url,
+        :Provider => provider
+    }
+    result[:pay_hash] = Digest::SHA1.hexdigest(result[:pay_amount] + result[:pay_order] + result[:pay_goal] +
+                                                   result[:pay_success_url] + result[:pay_fail_url] +
+                                                   result[:pay_result_url] + result[:Provider] + secret_key)
+    result
+  end
+
+  private
+
+  def validate!
+    raise Exception.new('pay_amount is required') if pay_amount.nil?
+    raise Exception.new('pay_amount is not a number') unless pay_amount.is_a? Numeric
+    raise Exception.new('pay_amount must be greater than 0') if pay_amount.to_f <= 0
+    raise Exception.new('pay_order is required') if pay_order.nil? || pay_order.empty?
+    raise Exception.new('pay_goal is required') if pay_goal.nil? || pay_goal.empty?
+    raise Exception.new('pay_success_url is required') if pay_success_url.nil? || pay_success_url.empty?
+    raise Exception.new('pay_success_url is invalid') unless valid_url?(pay_success_url)
+    raise Exception.new('pay_fail_url is required') if pay_fail_url.nil? || pay_fail_url.empty?
+    raise Exception.new('pay_fail_url is invalid') unless valid_url?(pay_fail_url)
+    raise Exception.new('pay_result_url is required') if pay_result_url.nil? || pay_result_url.empty?
+    raise Exception.new('pay_result_url is invalid') unless valid_url?(pay_result_url)
+    raise Exception.new('provider is required') if provider.nil? || provider.empty?
+    raise Exception.new('secret_key is required') if secret_key.nil? || secret_key.empty?
+  end
+
+  def valid_url?(url)
+    uri = URI.parse(url)
+    uri.kind_of?(URI::HTTP) || uri.kind_of?(URI::HTTPS)
+  rescue URI::InvalidURIError
+    false
+  end
+
+  def hashify
+
+  end
 end
--- spec/models/request_spec.rb
+++ spec/models/request_spec.rb
@@ -2,4 +2,188 @@ require 'spec_helper'
 
 describe FreebankPayment::Request do
 
+  it 'rejects if payment amount is not a number' do
+    request = FreebankPayment::Request.new
+    request.pay_amount = 'wrong'
+    lambda {
+      request.params
+    }.should raise_error(Exception, 'pay_amount is not a number')
+  end
+
+  it 'rejects if payment amount is zero' do
+    request = FreebankPayment::Request.new
+    request.pay_amount = 0.0
+    lambda {
+      request.params
+    }.should raise_error(Exception, 'pay_amount must be greater than 0')
+  end
+
+  it 'rejects if payment amount is negative' do
+    request = FreebankPayment::Request.new
+    request.pay_amount = -1
+    lambda {
+      request.params
+    }.should raise_error(Exception, 'pay_amount must be greater than 0')
+  end
+
+  it 'rejects if payment order is not set' do
+    request = FreebankPayment::Request.new
+    request.pay_amount = 10
+    lambda {
+      request.params
+    }.should raise_error(Exception, 'pay_order is required')
+
+    request.pay_order = ''
+    lambda {
+      request.params
+    }.should raise_error(Exception, 'pay_order is required')
+  end
+
+  it 'rejects if payment goal is not set' do
+    request = FreebankPayment::Request.new
+    request.pay_amount = 10
+    request.pay_order = '1234567890'
+    lambda {
+      request.params
+    }.should raise_error(Exception, 'pay_goal is required')
+
+    request.pay_goal = ''
+    lambda {
+      request.params
+    }.should raise_error(Exception, 'pay_goal is required')
+  end
+
+  it 'rejects if success url is not set' do
+    request = FreebankPayment::Request.new
+    request.pay_amount = 10
+    request.pay_order = '1234567890'
+    request.pay_goal = 'Best Product'
+    lambda {
+      request.params
+    }.should raise_error(Exception, 'pay_success_url is required')
+
+    request.pay_success_url = ''
+    lambda {
+      request.params
+    }.should raise_error(Exception, 'pay_success_url is required')
+  end
+
+  it 'rejects if success url is not valid' do
+    request = FreebankPayment::Request.new
+    request.pay_amount = 10
+    request.pay_order = '1234567890'
+    request.pay_goal = 'Best Product'
+    request.pay_success_url = 'example.com' # without http://
+    lambda {
+      request.params
+    }.should raise_error(Exception, 'pay_success_url is invalid')
+  end
+
+  it 'rejects if fail url is not set' do
+    request = FreebankPayment::Request.new
+    request.pay_amount = 10
+    request.pay_order = '1234567890'
+    request.pay_goal = 'Best Product'
+    request.pay_success_url = 'http://example.com/'
+    lambda {
+      request.params
+    }.should raise_error(Exception, 'pay_fail_url is required')
+
+    request.pay_fail_url = ''
+    lambda {
+      request.params
+    }.should raise_error(Exception, 'pay_fail_url is required')
+  end
+
+  it 'rejects if fail url is not valid' do
+    request = FreebankPayment::Request.new
+    request.pay_amount = 10
+    request.pay_order = '1234567890'
+    request.pay_goal = 'Best Product'
+    request.pay_success_url = 'http://example.com'
+    request.pay_fail_url = 'example.com'
+    lambda {
+      request.params
+    }.should raise_error(Exception, 'pay_fail_url is invalid')
+  end
+
+  it 'rejects if result url is not set' do
+    request = FreebankPayment::Request.new
+    request.pay_amount = 10
+    request.pay_order = '1234567890'
+    request.pay_goal = 'Best Product'
+    request.pay_success_url = 'http://example.com/'
+    request.pay_fail_url = 'http://example.com/'
+    lambda {
+      request.params
+    }.should raise_error(Exception, 'pay_result_url is required')
+
+    request.pay_result_url = ''
+    lambda {
+      request.params
+    }.should raise_error(Exception, 'pay_result_url is required')
+  end
+
+  it 'rejects if result url is not valid' do
+    request = FreebankPayment::Request.new
+    request.pay_amount = 10
+    request.pay_order = '1234567890'
+    request.pay_goal = 'Best Product'
+    request.pay_success_url = 'http://example.com'
+    request.pay_fail_url = 'http://example.com'
+    request.pay_result_url = 'example.com'
+    lambda {
+      request.params
+    }.should raise_error(Exception, 'pay_result_url is invalid')
+  end
+
+  it 'rejects if provider is not set' do
+    request = FreebankPayment::Request.new
+    request.pay_amount = 10
+    request.pay_order = '1234567890'
+    request.pay_goal = 'Best Product'
+    request.pay_success_url = 'http://example.com/'
+    request.pay_fail_url = 'http://example.com/'
+    request.pay_result_url = 'http://example.com/'
+    lambda {
+      request.params
+    }.should raise_error(Exception, 'provider is required')
+
+    request.provider = ''
+    lambda {
+      request.params
+    }.should raise_error(Exception, 'provider is required')
+  end
+
+  it 'rejects if secret key is not set' do
+      request = FreebankPayment::Request.new
+      request.pay_amount = 10
+      request.pay_order = '1234567890'
+      request.pay_goal = 'Best Product'
+      request.pay_success_url = 'http://example.com/'
+      request.pay_fail_url = 'http://example.com/'
+      request.pay_result_url = 'http://example.com/'
+      request.provider = '1234567890'
+      lambda {
+        request.params
+      }.should raise_error(Exception, 'secret_key is required')
+
+      request.secret_key = ''
+      lambda {
+        request.params
+      }.should raise_error(Exception, 'secret_key is required')
+    end
+
+  it 'creates valid hash' do
+    request = FreebankPayment::Request.new
+    request.pay_amount = 10
+    request.pay_order = '1234567890'
+    request.pay_goal = 'Best Product'
+    request.pay_success_url = 'http://example.com/'
+    request.pay_fail_url = 'http://example.com/'
+    request.pay_result_url = 'http://example.com/'
+    request.provider = '1234567890'
+    request.secret_key = '1234'
+    request.params[:pay_hash].should == '72b0cf3466e8e06ded9201b04f81795452f0169a'
+  end
 end
